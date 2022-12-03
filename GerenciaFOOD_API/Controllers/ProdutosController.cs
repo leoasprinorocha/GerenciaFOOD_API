8d0a441c3792ec395b2b6c68f761f5e2eeba1be2
LeonardoAsprinoRocha <asprinoufmscg@outlook.com>
2022-12-02 21:13:38 -0400

    Teste jenkins

--- GerenciaFOOD_API/Controllers/ProdutosController.cs
+++ GerenciaFOOD_API/Controllers/ProdutosController.cs
@@ -20,6 +20,7 @@ namespace GerenciaFOOD_API.Controllers
             products.Add(new ProdutoMock() { Id = 2, Name = "Suco" });
             products.Add(new ProdutoMock() { Id = 3, Name = "Pizza" });
             products.Add(new ProdutoMock() { Id = 4, Name = "Churros" });
+            products.Add(new ProdutoMock() { Id = 5, Name = "Churros" });
 
             return Ok(products);
 
